555f9b6db52e0625c02ea56ae2b0788df79280bc
shigeru <hironoki3@gmail.com>
2022-02-06 20:24:56 +0900

    GUI Timer

--- ui_demo/src/main.rs
+++ ui_demo/src/main.rs
@@ -1,6 +1,15 @@
-use iced::{executor, Application, Command, Element, Text, Settings};
+use iced::{button, executor, Align, Application, Button, Column, Command, Element, Font, HorizontalAlignment, Length, Row, Settings, Subscription, Text};
+use iced::Font::External;
 
-struct GUI;
+const FONT: Font = External {
+	name: "PxelMplus12-Regular",
+	bytes: include_bytes!("../rsc/PixelMplus12-Regular.ttf"),
+};
+
+struct GUI {
+	start_stop_button_state: button::State,
+	reset_button_state: button::State,
+}
 
 impl Application for GUI {
 	type Executor = executor::Null;
@@ -8,7 +17,13 @@ impl Application for GUI {
 	type Flags = ();
 
 	fn new(_flags: ()) -> (GUI, Command<Self::Message>) {
-		(GUI, Command::none())
+		(
+			GUI {
+				start_stop_button_state: button::State::new(),
+				reset_button_state: button::State::new(),
+			},
+			Command::none()
+		)
 	}
 
 	fn title(&self) -> String {
@@ -20,10 +35,29 @@ impl Application for GUI {
 	}
 
 	fn view(&mut self) -> Element<Self::Message> {
-		Text::new("Hello, World!").into()
+		let tick_text = Text::new("00:00:00.00").font(FONT).size(60);
+		let start_stop_button = Button::new(
+			&mut self.start_stop_button_state,
+			Text::new("Start").horizontal_alignment(HorizontalAlignment::Center).font(FONT),
+		).min_width(80);
+		let reset_button = Button::new(
+			&mut self.reset_button_state,
+			Text::new("Reset").horizontal_alignment(HorizontalAlignment::Center).font(FONT),
+		).min_width(80);
+
+		Column::new().push(tick_text).push(
+			Row::new().push(start_stop_button).push(reset_button).spacing(10),
+		).spacing(10)
+			.padding(10)
+			.width(Length::Fill)
+			.height(Length::Fill)
+			.align_items(Align::Center)
+			.into()
 	}
 }
 
 fn main() {
-	GUI::run(Settings::default());
+	let mut settings = Settings::default();
+	settings.window.size = (400u32, 120u32);
+	GUI::run(settings);
 }
